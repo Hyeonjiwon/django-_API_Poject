70aae26e54eca0d647ada5c9183216c431cd2f9e
Hyeonjiwon <zlone1208@gmail.com>
2019-10-30 11:48:51 +0900

    first commit

---
+++ mystorage/views.py
@@ -0,0 +1,26 @@
+from rest_framework import viewsets
+from .models import Essay
+from .serializers import EssaySerializer
+from rest_framework.filters import SearchFilter
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Essay.objects.all()
+    serializer_class = EssaySerializer
+
+    #검색 필터
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'body')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    #queryset filtering
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author = self.request.user)
+        else:
+            qs = qs.none()
+
+        return qs
