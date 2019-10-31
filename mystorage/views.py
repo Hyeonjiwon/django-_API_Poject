cd723217e491bf595ee2af41e767d4ed2d8131f0
Hyeonjiwon <zlone1208@gmail.com>
2019-10-31 17:28:26 +0900

    third commit

--- mystorage/views.py
+++ mystorage/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
-from .models import Essay
-from .serializers import EssaySerializer
+from .models import Essay, Album, Files
+from .serializers import EssaySerializer, AlbumSerializer, FilesSerializer
 from rest_framework.filters import SearchFilter
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Essay.objects.all()
@@ -24,3 +27,24 @@ class PostViewSet(viewsets.ModelViewSet):
             qs = qs.none()
 
         return qs
+
+class ImgViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = Files.objects.all()
+    serializer_class = FilesSerializer
+
+    #parser_class 지정
+    parser_classes = (MultiPartParser, FormParser)
+    
+    #create() 오버라이딩
+    def post(self, request, *args, **kwargs):
+        serializer = FilesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        
+        else:
+            return Respones(serializer.error, status=HTTP_400_BAD_REQUEST)
